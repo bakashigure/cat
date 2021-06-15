2a7a4c19bffeb16bf20135cde465a5766d5af48b
bakashigure <bakashigure@hotmail.com>
2021-06-16 00:17:07 +0800

    date - 210616

--- src/Banner.tsx
+++ src/Banner.tsx
@@ -7,9 +7,8 @@ let banner_title = <p> Catalog, log a cat.</p>;
 class Banner extends Component <any>{
     render() {
         return (
-            <div id="banner2" className="Banner">
-
-                    <div id="test" className={this.props.className} >
+            <div id="banner" className="Banner">
+                    <div id="banner_title" className="banner_title" >
                         {banner_title}
                     </div>
 
--- src/scroll.tsx
+++ src/scroll.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
 import jquery from 'jquery';
-declare var $: any;
-function test() {
 
+declare var $: any;
+
+function test() {
     $(window).scroll(function () {
         let scrollTop: number = $(window).scrollTop();
-        if (scrollTop.toString()=="0"){
+        if (scrollTop >0) {
             console.log("yes!")
-            $("#banner3").animate({
-                height:"50px",
+            $("#banner").animate({
+                    height: "40px",
+
                 }
             )
+            $("#banner_title").animate({
+
+
+                    'font-size':"50px",
+
+                }
+            )
+            $("#banner_title").css("color","red")
+
+        } else if (scrollTop == 0) {
+            console.log("set >= 500")
+            $("#banner2").animate({
+                height: "100px",
+            })
         }
 
         console.log((scrollTop))
     });
 }
+
 export default test
